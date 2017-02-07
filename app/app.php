0d017a35ebd9cc39978c092f596615155b00fa63
Andrew Devlin and Jason  Brown <andrew@>
2017-02-07 13:40:58 -0800

    initialize base files with creattion of Task.php with setters and getters

---
+++ app/app.php
@@ -0,0 +1,28 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
+
+    $app = new Silex\Application();
+
+    $app->get("/", function() {
+        
+
+        $output = "";
+
+        foreach ($list_of_tasks as $task) {
+            $output = $output . "<p>" . $task->getDescription() . "<p>";
+        }
+
+        return $output;
+    });
+
+    return $app;
+
+?>
